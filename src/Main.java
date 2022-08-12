7d06daf1177c1bbb52e3265e9c3ce3760e95a37d
Vadim Basalaev <basalaev.vad@yandex.ru>
2022-08-12 12:14:45 +0400

    Innitial Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[]sales = new int[]{1,3,7,9,3,100,50};
+
+        SalesManager Manager =new SalesManager(sales);
+        {
+            System.out.println(Manager.max());
+
+        }
+
+    }
+}
